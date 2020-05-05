493866678449856e16df6b73f350b1dc89970642
BruceYoung19 <Bruce7645@gmail.com>
2020-05-06 09:29:01 +1200

    Backend programming for button

--- Lifter/app/src/main/java/com/example/lifter/WorkoutActivity.java
+++ Lifter/app/src/main/java/com/example/lifter/WorkoutActivity.java
@@ -2,13 +2,39 @@ package com.example.lifter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class WorkoutActivity extends AppCompatActivity {
+public class WorkoutActivity extends AppCompatActivity implements View.OnClickListener  {
+
+    // objects
+    private Button finbutton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout);
+
+        finbutton = (Button)findViewById(R.id.Finishworkbutton);
+        finbutton.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        Intent workoutintent;
+        // switch case for the buttons
+        switch(v.getId()){
+            case R.id.Finishworkbutton:
+                Toast.makeText(this,"Finished workout",Toast.LENGTH_SHORT).show();
+        }
+
+
+
     }
 }
