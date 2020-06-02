05bd5767a3a2ef238146ec7a90df0ecd2fdcf5e4
Harpreet Singh <harpreet.kairon007@gmail.com>
2020-06-02 23:08:26 +1200

    View photos on a scroll bar.

---
+++ Lifter/app/src/main/java/com/example/lifter/progress/PhotoRepository.java
@@ -0,0 +1,141 @@
+package com.example.lifter.progress;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Environment;
+
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * This class is responsible for load all images inside the directory of the app.
+ **/
+public class PhotoRepository {
+
+    /**
+     * @return This method will return the number of photos taken by the user.
+     */
+    public int getPhotoCounts(Context context) {
+        // Find all the photos saved
+        List<Photo> photos = getPhotosFromDeviceStorage(context);
+        int numberOfPhotos = photos.size();
+        return numberOfPhotos;
+    }
+
+    /**
+     * @return This method will return all the photos the user currently has taken and which have
+     * been saved to the device.
+     */
+    public List<Photo> getPhotosFromDeviceStorage(Context context) {
+        // Get the reference to the Android/data/com.example.lifter/files/Pictures folder.
+        File folder = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+        // Get all the files inside the Android/data/com.example.lifter/files/Pictures folder.
+        File[] files = folder.listFiles();
+
+        // Photo response
+        List<Photo> photos = new ArrayList<Photo>();
+
+        // Loop through all the files photos inside Android/data/com.example.lifter/files/Pictures
+        // folder and add them into our photos array list.
+        for (File pic : files) {
+
+            // Find the path of our photo.
+            Uri photoPath = Uri.fromFile(pic);
+
+            // Find the date when the photo was last created we will show in the UI.
+            SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss a");
+            Date lastModDate = new Date(pic.lastModified());
+
+            // Convert the date object into string
+            String date = format.format(lastModDate);
+
+            // Finally add the data into our photo object so that we can return it.
+            Photo photo = new Photo(photoPath, date);
+            photos.add(photo);
+        }
+        return photos;
+    }
+
+    /**
+     * This method deletes photos
+     *
+     * @param photoToDelete this is the photo to be deleted.
+     * @return Returns return if the photo is deleted or else false.
+     */
+    public boolean deletePhoto(Photo photoToDelete) {
+
+        // Get the file path of the photo we going to delete.
+        File file = new File(photoToDelete.photoPath.getPath());
+
+        // Check if photo exist.
+        boolean exist = file.exists();
+
+        // if photo does not exist we have to return false so that inside the progress activity
+        // we can display that photo could not be deleted due to system issues.
+        if (exist == false) return false;
+
+        // Keep track if the photo is deleted
+        boolean deleted = false;
+
+        // If the photo exist we delete it.
+        if (exist) {
+            // try delete the photo and store if the photo is deleted into the deleted variable.
+            deleted = file.delete();
+        }
+
+        // return whether photo was deleted.
+        return deleted;
+    }
+
+    /**
+     * This method creates a blank file where the photo will be saved to.
+     * When the user takes the photo the blank file will be filled with the photo.
+     * NOTE - If the camera app is launched and photo is not taken. The blank file
+     * will be displayed inside the Progress activity.
+     *
+     * @return URI of the file which the photo should be saved to.
+     */
+    public Uri createFile(Context context) {
+        // Create the File where the photo should go
+        File photoFile = null;
+        try {
+            // Generate the file name using a time stamp. Note file name must be unique, we are
+            // using datetime to represent the file name this will guarantee a unique name.
+            String timeStamp = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss").format(new Date());
+            File storageDir = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+            photoFile = File.createTempFile(
+                    "" + timeStamp + "",  /* prefix */
+                    ".jpg",         /* suffix */
+                    storageDir      /* directory */
+            );
+
+        } catch (IOException ex) {
+            // Error occurred while creating the File
+            ex.printStackTrace();
+        }
+
+        // Get the path of the newly created blank photo.
+        Uri photoURI = FileProvider.getUriForFile(context, "com.example.lifter", photoFile);
+        return photoURI;
+    }
+
+    /**
+     * This method deletes all the photos that are saved on the device.
+     */
+    public void deleteAllPhotos(Context context){
+        // Get all the photos on the device
+        List<Photo> photos = getPhotosFromDeviceStorage(context);
+
+        // Delete all photos 1 by 1.
+        for (Photo photo: photos) {
+            deletePhoto(photo);
+        }
+    }
+}
---
+++ Lifter/app/src/main/java/com/example/lifter/progress/ProgressActivity.java
@@ -0,0 +1,157 @@
+package com.example.lifter.progress;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.lifter.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProgressActivity extends AppCompatActivity implements ProgressPhotoGalleryFragment.OnListFragmentInteractionListener {
+
+    private RecyclerView.Adapter recyclerViewAdapter;
+    private RecyclerView recyclerView;
+    private FloatingActionButton buttonTakePhoto;
+    private static final int REQUEST_TAKE_PHOTO = 1;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_STORAGE_PERMISSION = 1;
+    private MyProgressPhotoGalleryRecyclerViewAdapter adapter;
+    private List<Photo> photos = new ArrayList<Photo>();
+    private PhotoRepository photoRepository = new PhotoRepository();
+    private ImageView thumbnailImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_progress);
+        buttonTakePhoto = findViewById(R.id.button_take_photo);
+        thumbnailImageView = findViewById(R.id.photo_thumbnail);
+
+        if (recyclerViewAdapter == null) {
+            Fragment currentFragment = getSupportFragmentManager().findFragmentById(R.id.main_fragment);
+            recyclerView = (RecyclerView) currentFragment.getView();
+            recyclerViewAdapter = ((RecyclerView) currentFragment.getView()).getAdapter();
+        }
+
+        adapter = new MyProgressPhotoGalleryRecyclerViewAdapter(photos, this);
+        recyclerView.setAdapter(adapter);
+        loadPhotos();
+        setThumbnail();
+
+        buttonTakePhoto.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // First we need to ask the user to give permission to save photo to external location
+                if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
+
+                    // If you do not have permission, request it
+                    ActivityCompat.requestPermissions(ProgressActivity.this,
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            REQUEST_STORAGE_PERMISSION);
+                } else {
+                    // When button_take_photo is clicked this method will run.
+                    takePicture();
+                }
+            }
+        });
+    }
+
+    /**
+     * Load images from storage and add them to the list view.
+     */
+    private void loadPhotos() {
+        // First clear all the photos we currently have.
+        photos.clear();
+
+        // Get photos from storage using repository
+        List<Photo> photoList = photoRepository.getPhotosFromDeviceStorage(this);
+
+        // Add all the photos to list which is used inside the UI.
+        photos.addAll(photoList);
+
+        // Notify the UI that photo has been added so that it can display them.
+        adapter.notifyDataSetChanged();
+    }
+
+    /*
+     * Sets the Thumbnail photo
+     **/
+    private void setThumbnail(){
+        // If we have photos then show the first photo as the thumbnail
+        if(photos.size() >= 1){
+            Photo firstPhoto = photos.get(0);
+            // Set the first photo as thumbnail
+            thumbnailImageView.setImageURI(firstPhoto.photoPath);
+        }
+    }
+
+    @Override
+    public void onListFragmentInteraction(final Photo item) {
+        thumbnailImageView.setImageURI(item.photoPath);
+
+      /*  new AlertDialog.Builder(this)
+                .setTitle("Progress Photo")
+                .setMessage("Delete this progress photo?")
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Try delete the photo and store if it was deleted into a new variable
+                        boolean deleted = photoRepository.deletePhoto(item);
+
+                        // If photo was deleted notify the user and refresh the photo list.
+                        if (deleted) {
+                            Toast.makeText(ProgressActivity.this, "Progress Photo Deleted.", Toast.LENGTH_SHORT).show();
+                            // Refresh photo view once the photo has been deleted
+                            loadPhotos();
+                        } else {
+                            // Show error message to user whether we have successfully deleted the photo.
+                            Toast.makeText(ProgressActivity.this, "Could not delete Progress Photo.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                })
+                .setNegativeButton(android.R.string.no, null).show();*/
+    }
+
+    // This method will show the camera app and allow you to take a photo.
+    private void takePicture() {
+        // The camera app is launched using a intent
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+
+            Uri photoUri = photoRepository.createFile(this);
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+
+            // Launch the camera and start taking photo. Once the user comes back to our app from camera app.
+            // The onActivityResult will be called.
+            startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // When the photo is taken by the camera app this method will be called
+        // And we will take the image and show it on the image view in our activity.
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+
+            // reload the images and update the list.
+            loadPhotos();
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
