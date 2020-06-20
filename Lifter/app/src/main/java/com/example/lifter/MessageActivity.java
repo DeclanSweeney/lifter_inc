2353f89cadccc88f44cf99ce06eff9c2bd055ed9
Declan <declan.sweeneyNZDF@gmail.com>
2020-06-20 19:16:50 +1200

    Added toolbar to MessageActivity with return to ChatActivity

--- Lifter/app/src/main/java/com/example/lifter/MessageActivity.java
+++ Lifter/app/src/main/java/com/example/lifter/MessageActivity.java
@@ -1,12 +1,25 @@
 package com.example.lifter;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MessageActivity extends AppCompatActivity {
     private String messageRequestID, messageRequestName;
+
+    private TextView username;
+    private CircleImageView profilePic;
+    private Toolbar messageToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,8 +28,24 @@ public class MessageActivity extends AppCompatActivity {
         messageRequestID = getIntent().getExtras().get("message_user_id").toString();
         messageRequestName = getIntent().getExtras().get("message_user_name").toString();
 
-        Toast.makeText(this, messageRequestID, Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, messageRequestName, Toast.LENGTH_SHORT).show();
+        InitializeFields();
 
+        username.setText(messageRequestName);
+    }
+
+    private void InitializeFields() {
+        messageToolbar = (Toolbar) findViewById(R.id.message_toolbar);
+        setSupportActionBar(messageToolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowCustomEnabled(true);
+
+        LayoutInflater layoutInflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View actionBarView = layoutInflater.inflate(R.layout.custom_chat_bar, null);
+        actionBar.setCustomView(actionBarView);
+
+        profilePic = findViewById(R.id.custom_profile_image);
+        username = findViewById(R.id.custom_profile_name);
     }
 }
